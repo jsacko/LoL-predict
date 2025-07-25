c9ec0928f896318294c7e0aeeead92acf6bf479b
jsacko <63580474+jsacko@users.noreply.github.com>
2025-07-24 23:58:43 -0700

    test run

--- run.sh
+++ run.sh
@@ -11,6 +11,8 @@ sleep 10  # attendre que le serveur MLflow démarre
 
 # Exécuter l'entraînement
 echo "Running pipeline..."
+echo SUPABASE_URL $SUPABASE_URL
+echo SUPABASE_KEY $SUPABASE_KEY
 python src/pipelines/pipeline.py
 
 # Terminer proprement
